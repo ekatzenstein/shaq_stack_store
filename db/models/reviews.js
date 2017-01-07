8e00c9e88c5f6df840a4ad6f3bb49f08b2b16f19
tvvinkle <arumbit@gmail.com>
2017-01-07 17:36:42 -0500

    review model

---
+++ db/models/reviews.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const bcrypt = require('bcrypt')
+const Sequelize = require('sequelize')
+const db = require('APP/db')
+
+const Reviews = db.define('reviews', {
+  rating: {
+  	type: Sequelize.INTEGER,
+  	allowNull: false,
+  	defaultValue: 5,
+    validate: {
+			notEmpty: true,
+			isNumeric: true,
+			max: 5,
+			min: 1,
+		}
+  },
+
+  review_text: {
+  	type: Sequelize.TEXT
+  	allowNull: false,
+  	defaultValue: null,
+  	validate: {
+  		notEmpty: true,
+  	}
+  },  
+  
+
+
+}
+
+module.exports = Reviews;
+
+
+// reviews_id (pk)
+// users_id (fk)
+// product_id (fk)
+// review_text 
+// ratings - default value 0
